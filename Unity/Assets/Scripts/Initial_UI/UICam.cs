7b7591fc4b275ec149d9dc567445dc1f494490ae
fernandolv123 <ferlorenzovazquez@gmail.com>
2024-02-08 04:17:31 +0100

    Interfaz de usuario terminada Close#11

--- Unity/Assets/Scripts/Initial_UI/UICam.cs
+++ Unity/Assets/Scripts/Initial_UI/UICam.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UICam : MonoBehaviour
 {
+    public Image sprite;
+    [Header("This contains variables related to camera movement")]
+    [SerializeField]
+    private Transform camTeleporter;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,26 @@ public class UICam : MonoBehaviour
     void Update()
     {
         transform.Translate(-Vector3.right*2*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        StartCoroutine("FadeOut");
+        Debug.Log("Entra");
+        
+    }
+
+    IEnumerator FadeOut(){
+        yield return null;
+        for (float i = 0; i <= 1f; i += Time.deltaTime)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, i);
+            yield return null;
+        }
+        transform.position = camTeleporter.position;
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, i);
+            yield return null;
+        }
     }
 }
